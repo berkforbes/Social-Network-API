c02be8aa7f106daca8d0c3d17c3291970dc53f21
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-09 19:33:46 -0700

    added call for virtuals and getters

--- models/User.js
+++ models/User.js
@@ -1,36 +1,47 @@
 const { Schema, model } = require("mongoose");
 // import for email validator
-import { isEmail } from 'validator';
+import { isEmail } from "validator";
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    vaidate: [isEmail, "Please enter a valid email address"],
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      vaidate: [isEmail, "Please enter a valid email address"],
     },
-  ],
-});
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // retrieve the length of users friends array
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.reduce((total, friend) =>
-    total + friend.length +1, 0)
-})
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+});
+
+// export the User model
+module.exports = User;
