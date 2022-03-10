760fa5f1cf8bf3f88b912c9c99eebba2d96ee7eb
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-09 20:14:28 -0700

    thought route

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  addThought,
+  getThoughtById,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+const { route } = require("./user-routes");
+
+router.route("/").get(getAllThoughts);
+
+router.route("/:userId").post(addThought);
+
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
+
+router.route("/:userId/:thoughtId").delete(removeThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+module.exports = router;
