a8b02eb158b4e900b1f3a9e50531d2d5425f3726
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-09 20:15:29 -0700

    added delete reaction

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -21,4 +21,6 @@ router.route("/:userId/:thoughtId").delete(removeThought);
 
 router.route("/:thoughtId/reactions").post(addReaction);
 
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
 module.exports = router;
