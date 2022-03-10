d630afe24622feb1f95414db4af2dc09b89e8d8c
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-09 19:54:47 -0700

    reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,40 +1,39 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
-const reactionSchema = require('./Reaction');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const reactionSchema = require("./Reaction");
 
-// thought schema
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 // virtual to get reaction count
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = { Thought };
