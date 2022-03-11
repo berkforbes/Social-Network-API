18de3f0209851d1aece150a0ccb967204b9246d7
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-10 17:47:48 -0700

    get user by id, create user, update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,5 +16,40 @@ const userController = {
         console.log(err);
         res.sendStatus(400);
       });
+  },
+
+  // Get User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // Create User
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Update User 
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id.' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   }
 }
